9b5a11e90420e6d8bcb6a5a69cc19f30ec817590
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2024-06-30 17:30:20 +0300

    main branch change

--- hw01-git/src/main/java/guru/qa/Main.java
+++ hw01-git/src/main/java/guru/qa/Main.java
@@ -3,6 +3,6 @@ package guru.qa;
 @SuppressWarnings("java:S106")
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world!");
     }
 }
