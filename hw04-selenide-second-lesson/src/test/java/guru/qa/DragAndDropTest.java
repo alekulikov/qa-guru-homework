305b7630b0f13203316958949c95854edfd94077
alekulikov <99183267+alekulikov@users.noreply.github.com>
2024-07-27 12:11:09 +0300

    hw04-selenide-second-lesson (#4)
    * add hw04-selenide-second-lesson
    
    * Update GitHubEnterpriseTest.java
    
    ---------
    
    Co-authored-by: alekulikov <aleksandr.a.kulikov@mvideo.ru>

---
+++ hw04-selenide-second-lesson/src/test/java/guru/qa/DragAndDropTest.java
@@ -0,0 +1,44 @@
+package guru.qa;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.*;
+
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class DragAndDropTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    void dragAndDropWithActionTest() {
+        open("/drag_and_drop");
+        actions()
+                .moveToElement($(byId("column-a")))
+                .clickAndHold()
+                .moveToElement($(byId("column-b")))
+                .release()
+                .perform();
+        $(byId("column-a")).shouldHave(text("B"));
+        $(byId("column-b")).shouldHave(text("A"));
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("/drag_and_drop");
+        $(byId("column-a")).dragAndDrop(to($(byId("column-b"))));
+        $(byId("column-a")).shouldHave(text("B"));
+        $(byId("column-b")).shouldHave(text("A"));
+    }
+}
